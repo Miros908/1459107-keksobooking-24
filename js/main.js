2502a4649262adc707a4447629bb2770a22e666e
ваше имя <84718361+Miros908@users.noreply.github.com>
2021-09-25 19:29:32 +0300

    Исправил stylelint

--- js/main.js
+++ js/main.js
@@ -1,25 +1,29 @@
 const getRandomIntInclusive = function(min, max) {
-if(min>max || min==max){
-  console.log('в заданом интервале нет целых чисел');
-}else{
-  min = Math.ceil(min);//округляем на случай если min<1
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+  if(min>max || min === max){
+    throw('в заданом интервале нет целых чисел');
+  }else{
+    min = Math.ceil(min);//округляем на случай если min<1
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
-}
+};
 
-getRandomIntInclusive(5,10);
+getRandomIntInclusive();
 
 
 function getRandomArbitrary(min, max) {
 
-  if(min>max || min==max){
-    console.log('в заданом интервале нет целых чисел');
+  if(min>max || min === max){
+    throw('в заданом интервале нет целых чисел');
   }else{
-  const RandomNumber=(Math.random() * (max - min) + min);
-  return RandomNumber.toFixed(1);
+    const RandomNumber=(Math.random() * (max - min) + min);
+    return RandomNumber.toFixed(1);
+
   }
+
 }
 
+
+getRandomArbitrary();
 
 
