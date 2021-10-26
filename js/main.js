2e7f224da315458128f82e4d31b6790fc8a808ba
ваше имя <84718361+Miros908@users.noreply.github.com>
2021-10-26 18:48:18 +0300

    ...

--- js/main.js
+++ js/main.js
@@ -1,4 +1,43 @@
-import{minsymbols} from './form.js';
-minsymbols();
-import { marker } from './get-map.js';
+const forms=document.querySelector('.ad-form');
+const formfilter=document.querySelector('.map__filters');
+const title=forms.querySelector('.titles');
+const type=forms.querySelector('#type');
+const price=forms.querySelector('.prices');
+const room=forms.querySelector('#room_number');
+const guest=forms.querySelector('#capacity');
+const timeout=forms.querySelector('#timeout');
+const timein=forms.querySelector('#timein');
+const time=forms.querySelector('.ad-form__element--time');
 
+
+import{minsymbols} from './form.js';//Минимальное количество симоволов для обьявления
+import{priceForType} from'./setting.js';//цена в зависисмости от типа жилья
+import{getPlaceholder} from './form.js';//изменения плейсхолдера
+import{getMinprice} from './form.js';//изменение минимальной цены
+import{settings} from './setting.js';//настройки заказчика
+import { GetRoom } from './form.js';//количество человек в зависимости от комнат
+import{disabledpage} from './disabled-page.js';
+import{activepage} from './disabled-page.js';
+
+
+
+
+minsymbols(title);
+
+type.addEventListener('change',()=> {
+  getPlaceholder(priceForType,type,price);
+  getMinprice(price,priceForType,type);
+
+
+});
+
+
+room.addEventListener('change',()=> {
+  GetRoom(room,guest,settings);
+});
+
+time.addEventListener('change',(evt)=> {
+  timeout.value=evt.target.value;
+  timein.value=evt.target.value;
+
+});
---
+++ js/setting.js
@@ -0,0 +1,15 @@
+const priceForType={
+  bungalow:0,
+  flat:1000,
+  hotel:3000,
+  house:5000,
+  palace:10000,
+};//Цена по типу жилья
+
+const settings = {
+  '1': ['1'],
+  '2': ['1','2'],
+  '3': ['1','2','3'],
+  '100': ['0'],
+};//Количество человек в зависисмости от  номера
+export{priceForType,settings}
